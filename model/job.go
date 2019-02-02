dac19ee5be48c3c0864f48ec72b7ed21f6416a9c
Ryan <ryannitz@amazon.com>
2019-02-02 10:55:15 -0500

    Clean

--- model/job.go
+++ model/job.go
@@ -2,12 +2,17 @@ package model
 
 import (
 	"context"
+	"fmt"
 	"time"
 
+	log "github.com/golang/glog"
 	"github.com/mongodb/mongo-go-driver/bson"
 	"github.com/mongodb/mongo-go-driver/bson/primitive"
+	"github.com/mongodb/mongo-go-driver/mongo"
 	docdb "github.com/rnzsgh/fargate-documentdb-compute-poc/db"
 )
+
+const jobMaxFailureRetry = 3
 
 type Job struct {
 	Id            *primitive.ObjectID `json:"id" bson:"_id,omitempty"`
@@ -17,33 +22,64 @@ type Job struct {
 	Stop          *time.Time          `json:"stop,omitempty" bson:"stop,omitempty"`
 }
 
-func JobUpdateFailureReason(id *primitive.ObjectID, reason string) (err error) {
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-	_, err = docdb.Client.Database("work").Collection("jobs").UpdateOne(
-		ctx,
-		bson.D{{"_id", id}},
-		bson.D{{"$set", bson.D{{"failure", reason}}}})
-	return
+func JobUpdateFailureReason(id *primitive.ObjectID, reason string) error {
+	return jobUpdateField(id, "failure", reason)
 }
 
-func JobUpdateStopTime(id *primitive.ObjectID) (err error) {
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-	_, err = docdb.Client.Database("work").Collection("jobs").UpdateOne(
-		ctx,
-		bson.D{{"_id", id}},
-		bson.D{{"$set", bson.D{{"stop", time.Now()}}}})
-	return
+func JobUpdateStopTime(id *primitive.ObjectID) error {
+	return jobUpdateField(id, "stop", time.Now())
 }
 
 func JobCreate(job *Job) (err error) {
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-	_, err = docdb.Client.Database("work").Collection("jobs").InsertOne(ctx, job)
+	_, err = jobCollection().InsertOne(ctx, job)
 	return
 }
 
 func JobFindById(id *primitive.ObjectID) (job *Job, err error) {
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 	job = &Job{}
-	err = docdb.Client.Database("work").Collection("jobs").FindOne(ctx, bson.D{{"_id", id}}).Decode(job)
+	err = jobCollection().FindOne(ctx, bson.D{{"_id", id}}).Decode(job)
 	return
+}
+
+func jobCollection() *mongo.Collection {
+	return docdb.Client.Database("work").Collection("jobs")
+}
+
+func jobUpdateField(id *primitive.ObjectID, field string, value interface{}) error {
+	count := 0
+
+	for {
+		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+		res, err := jobCollection().UpdateOne(
+			ctx,
+			bson.D{{"_id", id}},
+			bson.D{{"$set", bson.D{{field, value}}}})
+
+		if err == nil {
+			if res.MatchedCount != 1 && res.ModifiedCount != 1 {
+				return fmt.Errorf(
+					"Job field not updated - job: %s - field: %s",
+					id.Hex(),
+					field,
+				)
+			}
+			return nil
+		}
+
+		log.Errorf(
+			"Job field not updated - job: %s - field: %s - reason %v",
+			id.Hex(),
+			field,
+			err,
+		)
+
+		count++
+
+		time.Sleep(time.Duration(count*2) * time.Second)
+		if count == jobMaxFailureRetry {
+			return err
+		}
+	}
 }
--- model/task.go
+++ model/task.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"time"
 
+	log "github.com/golang/glog"
 	"github.com/mongodb/mongo-go-driver/bson"
 	"github.com/mongodb/mongo-go-driver/bson/primitive"
-	docdb "github.com/rnzsgh/fargate-documentdb-compute-poc/db"
 )
+
+const taskMaxFailureRetry = 3
 
 type Task struct {
 	Id            *primitive.ObjectID `json:"id" bson:"id"`
@@ -19,24 +21,54 @@ type Task struct {
 	Stop          *time.Time          `json:"stop,omitempty" bson:"stop,omitempty"`
 }
 
-// Update a field in the specfic task
-func taskUpdateField(task *Task, field string, value interface{}) (err error) {
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-	_, err = docdb.Client.Database("work").Collection("jobs").UpdateOne(
-		ctx,
-		bson.D{{"_id", task.JobId}},
-		bson.D{{"$set", bson.D{{fmt.Sprintf("tasks.%s.%s", task.Id.Hex(), field), value}}}})
-	return
-}
-
 func TaskUpdateArn(task *Task, arn string) error {
 	return taskUpdateField(task, "arn", arn)
 }
 
-func TaskUpdateFailureReason(task *Task, reason string) (err error) {
+func TaskUpdateFailureReason(task *Task, reason string) error {
 	return taskUpdateField(task, "failure", reason)
 }
 
-func TaskUpdateStopTime(task *Task) (err error) {
+func TaskUpdateStopTime(task *Task) error {
 	return taskUpdateField(task, "stop", task.Stop)
+}
+
+func taskUpdateField(task *Task, field string, value interface{}) error {
+
+	count := 0
+
+	for {
+		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+		res, err := jobCollection().UpdateOne(
+			ctx,
+			bson.D{{"_id", task.JobId}},
+			bson.D{{"$set", bson.D{{fmt.Sprintf("tasks.%s.%s", task.Id.Hex(), field), value}}}})
+
+		if err == nil {
+			if res.MatchedCount != 1 && res.ModifiedCount != 1 {
+				return fmt.Errorf(
+					"Task field not updated - job: %s - task %s - field: %s",
+					task.JobId.Hex(),
+					task.Id.Hex(),
+					field,
+				)
+			}
+			return nil
+		}
+
+		log.Errorf(
+			"Task field not updated - job: %s - task %s - field: %s - reason %v",
+			task.JobId.Hex(),
+			task.Id.Hex(),
+			field,
+			err,
+		)
+
+		count++
+
+		time.Sleep(time.Duration(count*2) * time.Second)
+		if count == taskMaxFailureRetry {
+			return err
+		}
+	}
 }
