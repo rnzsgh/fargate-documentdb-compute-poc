20f67bce5720814db50d4d982f4ceb8d1d9eca61
Ryan <ryannitz@amazon.com>
2019-02-02 10:28:17 -0500

    Simplify

--- model/task.go
+++ model/task.go
@@ -14,24 +14,29 @@ type Task struct {
 	Id            *primitive.ObjectID `json:"id" bson:"id"`
 	JobId         *primitive.ObjectID `json:"jobId" bson:"jobId"`
 	FailureReason string              `json:"failure" bson:"failure"`
+	Arn           string              `json:"arn,omitempty" bson:"arn,omitempty"`
 	Start         *time.Time          `json:"start" bson:"start"`
 	Stop          *time.Time          `json:"stop,omitempty" bson:"stop,omitempty"`
 }
 
-func TaskUpdateFailureReason(task *Task, reason string) (err error) {
+// Update a field in the specfic task
+func taskUpdateField(task *Task, field string, value interface{}) (err error) {
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 	_, err = docdb.Client.Database("work").Collection("jobs").UpdateOne(
 		ctx,
 		bson.D{{"_id", task.JobId}},
-		bson.D{{"$set", bson.D{{fmt.Sprintf("tasks.%s.failure", task.Id.Hex()), reason}}}})
+		bson.D{{"$set", bson.D{{fmt.Sprintf("tasks.%s.%s", task.Id.Hex(), field), value}}}})
 	return
 }
 
+func TaskUpdateArn(task *Task, arn string) error {
+	return taskUpdateField(task, "arn", arn)
+}
+
+func TaskUpdateFailureReason(task *Task, reason string) (err error) {
+	return taskUpdateField(task, "failure", reason)
+}
+
 func TaskUpdateStopTime(task *Task) (err error) {
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-	_, err = docdb.Client.Database("work").Collection("jobs").UpdateOne(
-		ctx,
-		bson.D{{"_id", task.JobId}},
-		bson.D{{"$set", bson.D{{fmt.Sprintf("tasks.%s.stop", task.Id.Hex()), task.Stop}}}})
-	return
+	return taskUpdateField(task, "stop", task.Stop)
 }
