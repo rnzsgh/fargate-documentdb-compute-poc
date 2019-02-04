481c0125f82c5d6d98e5ece9c79d1efd9de1d864
Ryan <ryannitz@amazon.com>
2019-02-04 12:42:57 -0500

    Dev

--- api/job.go
+++ api/job.go
@@ -2,10 +2,12 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"time"
 
+	log "github.com/golang/glog"
 	"github.com/mongodb/mongo-go-driver/bson/primitive"
 	"github.com/rnzsgh/fargate-documentdb-compute-poc/model"
 	"github.com/rnzsgh/fargate-documentdb-compute-poc/work"
@@ -17,13 +19,31 @@ func init() {
 
 func JobStart(w http.ResponseWriter, r *http.Request) {
 	response := &response{}
+	w.Header().Set("Content-Type", "text/plain")
 
-	// Create an example job
+	if err := model.DataEnsureTest(); err != nil {
+		response.Message = fmt.Sprintf("Unable to ensure test data is created - reason: %v", err)
+		log.Errorf(response.Message)
+		out, _ := json.Marshal(response)
+		io.WriteString(w, string(out))
+		return
+	}
+
+	dataIds, err := model.DataFindAllIds()
+	if err != nil {
+		response.Message = fmt.Sprintf("Unable to load test data doc ids - reason: %v", err)
+		log.Errorf(response.Message)
+		out, _ := json.Marshal(response)
+		io.WriteString(w, string(out))
+		return
+	}
+
+	// Create an sample job
 	jobId := primitive.NewObjectID()
 	tasks := make(map[string]*model.Task)
-	for i := 0; i < 11; i++ {
+	for i := 0; i < len(dataIds); i++ {
 		taskId := primitive.NewObjectID()
-		tasks[taskId.Hex()] = &model.Task{Id: &taskId, JobId: &jobId}
+		tasks[taskId.Hex()] = &model.Task{Id: &taskId, JobId: &jobId, DataId: dataIds[i]}
 	}
 
 	now := time.Now()
@@ -37,7 +57,7 @@ func JobStart(w http.ResponseWriter, r *http.Request) {
 	response.Job = job
 
 	// Normally this would be application/json, but we don't want to prompt downloads
-	w.Header().Set("Content-Type", "text/plain")
+
 	out, _ := json.Marshal(response)
 	io.WriteString(w, string(out))
 }
