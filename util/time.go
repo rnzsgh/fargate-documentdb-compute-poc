5df82c6156ce1e6bab083c24588967fb4dd529f0
Ryan <ryannitz@amazon.com>
2019-02-09 12:23:20 -0500

    Add sleep func

--- util/time.go
+++ util/time.go
@@ -15,3 +15,14 @@ func NowTimeUtc() *time.Time {
 	time := time.Now().UTC()
 	return &time
 }
+
+// Sleeps for two seconds, then four, etc., each time the closure is
+// called. Closure function returns the count/times it has slept.
+func ExpoentialSleepSeconds() func() int {
+	count := 0
+	return func() int {
+		count++
+		time.Sleep(time.Duration(count*2) * time.Second)
+		return count
+	}
+}
