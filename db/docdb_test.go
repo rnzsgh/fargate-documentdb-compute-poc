ec21dc9f8f33fc32e76d1cbe243e5e49a755b470
Ryan <ryannitz@amazon.com>
2019-02-13 06:09:25 -0700

    Dev

--- db/docdb_test.go
+++ db/docdb_test.go
@@ -1,12 +1,13 @@
 package db
 
 import (
+	"context"
 	"testing"
 )
 
 func TestInit(t *testing.T) {
 	t.Run("TestInit", func(t *testing.T) {
-		if err := ping(); err != nil {
+		if err := Ping(context.TODO()); err != nil {
 			t.Errorf("Unable to ping db: %v", err)
 		}
 	})
