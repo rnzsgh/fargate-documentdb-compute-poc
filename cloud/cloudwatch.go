251b42eefd8529d61f9715557ca653be0c6e832c
Ryan <ryannitz@amazon.com>
2019-04-02 09:19:29 -0400

    Method to monitor and update metrics

--- cloud/cloudwatch.go
+++ cloud/cloudwatch.go
@@ -2,12 +2,43 @@ package cloud
 
 import (
 	"fmt"
+	"sync"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/cloudwatch"
+	log "github.com/golang/glog"
 	"github.com/rnzsgh/fargate-documentdb-compute-poc/util"
 )
+
+// MonitorPutMetricDataBuffer returns a function that allows for real time metric
+// writing, but buffers the value for the duration parameter passed. Every d duration,
+// the metric is written to CloudWatch. If there are errors, they are written to the log.
+func MonitorPutMetricDataBuffer(namespace, metric string, d time.Duration) func(float64) {
+
+	var value float64
+	var m sync.Mutex
+
+	go func() {
+		for {
+			time.Sleep(d)
+			m.Lock()
+			v := value
+			m.Unlock()
+
+			if err := MonitorPutMetricData(namespace, metric, v); err != nil {
+				log.Errorf("Problem with MonitorPutMetricData in MonitorPutMetricDataBuffer - reason: %v", err)
+			}
+		}
+	}()
+
+	return func(v float64) {
+		m.Lock()
+		value = v
+		m.Unlock()
+	}
+}
 
 func MonitorPutMetricData(namespace, metric string, value float64) error {
 	svc := cloudwatch.New(session.New())
